019150adb2bcf0de4afeda81cc338d6cca44557b
zhai_meng <614196108@qq.com>
2023-02-18 19:14:18 +0800

    第8章：定义标记类型Aware接口，实现感知容器对象

--- small-spring-step-08/src/test/java/bean/UserDao.java
+++ small-spring-step-08/src/test/java/bean/UserDao.java
@@ -8,9 +8,8 @@ public class UserDao {
 
     public void initDataMethod() {
         System.out.println("执行：init-method");
-        hashMap.put("10001", "小傅哥");
-        hashMap.put("10002", "八杯水");
-        hashMap.put("10003", "阿毛");
+        hashMap.put("10001", "盟哥");
+        hashMap.put("10003", "华仔");
     }
 
     public void destroyDataMethod() {
