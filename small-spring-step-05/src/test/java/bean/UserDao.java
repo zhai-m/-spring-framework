59d18b5785408287aeee6daeea1aa48c648d0c97
zhai_meng <614196108@qq.com>
2023-02-18 19:20:38 +0800

    第5章：资源加载器解析文件注册对象

--- small-spring-step-05/src/test/java/bean/UserDao.java
+++ small-spring-step-05/src/test/java/bean/UserDao.java
@@ -9,9 +9,8 @@ public class UserDao {
     private static Map<String, String> hashMap = new HashMap<>();
 
     static {
-        hashMap.put("10001", "小傅哥");
-        hashMap.put("10002", "八杯水");
-        hashMap.put("10003", "阿毛");
+        hashMap.put("10001", "盟哥");
+        hashMap.put("10002", "华子");
     }
 
     public String queryUserName(String uId) {
